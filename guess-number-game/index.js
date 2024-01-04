0c86655fb1b7499db55faa858b06f3706f86f7d9
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-04 21:12:04 +0000

    guess a number from 1 to 10

--- guess-number-game/index.js
+++ guess-number-game/index.js
@@ -6,8 +6,15 @@ const guess = document.querySelector("#number-input");
 const button = document.querySelector("#guess-button");
 const div = document.querySelector(".guess-div");
 const text = document.querySelector("#result");
-const tryAgain = `<button type="submit" id="try-again-button">Try again</button>`
+const tip = document.querySelector("#tip-message");
+const tryAgain = `<button type="submit" id="try-again-button" class="btn btn-primary">Try again</button>`
+const newGame = `<button type="submit" id="new-game-button" class="btn btn-primary">New game</button>`
 const numberGuessed = document.querySelector("#numbers-guessed");
+const answers = ["Wrong answer! Try again...", "No, not that number, try again!", "Well, not that one! Try again"]
+const gameOver = "I'm sorry, but you lost the game!"
+let count = 0;
+const tries = document.querySelector("#attempts");
+console.log(tries.textContent)
 // console.dir(div);
 // console.log(text);
 // console.log(button);
@@ -20,7 +27,7 @@ const youWon = () => {
   console.log("it's working!");
   guess.remove();
   button.remove();
-  text.textContent = "You won";
+  text.textContent = "Congratulations you won 🥳";
   div.insertAdjacentHTML("beforeend", tryAgain);
   const reset = document.querySelector("#try-again-button");
   reset.addEventListener("click", () => {
@@ -28,40 +35,67 @@ const youWon = () => {
     });
 };
 
+const youLost = () => {
+  guess.remove();
+  button.remove();
+  text.textContent = gameOver;
+  div.insertAdjacentHTML("beforeend", newGame);
+  const anotherGame = document.querySelector("#new-game-button");
+  anotherGame.addEventListener("click", () => {
+    location.reload();
+  });
+};
+
 const higher = () => {
-  console.log("higher");
-  console.log(parseInt(guess.value));
+  let number = parseInt(guess.value);
+  console.log(number);
+  text.textContent = answers[Math.floor(Math.random() * answers.length)];
+  if (randomNumb - number <= 5) {
+    tip.innerHTML = `<p>Just a little higher!</p>`
+    console.log("A little higher!");
+  } else {
+    tip.innerHTML = `<p>Higher!</p>`
+    console.log("higher");
+  };
   guess.value = "";
 };
 
 const lower = () => {
-  if (randomNumb - parseInt(guess.value) <= 5) {
+  let number = parseInt(guess.value);
+  console.log(number);
+  text.textContent = answers[Math.floor(Math.random() * answers.length)];
+  if (number - randomNumb <= 5) {
+    tip.innerHTML = `<p>Just a little lower!</p>`
     console.log("a little lower!");
   } else {
+    tip.innerHTML = `<p>Lower!</p>`
     console.log("lower");
   };
-};
-
-const youLost = () => {
-  const answers = ["Wrong answer! Try again...", "No, not that number, try again!", "Well, not that one! Try again"]
   guess.value = ""
-  text.textContent = answers[Math.floor(Math.random() * answers.length)];
 };
 
+const attempts = (count) => {
+  const numberOfTries = parseInt(tries.attributes[0].value);
+  tries.textContent = `You have ${numberOfTries - count} left!`
+}
 
-  button.addEventListener("click", () => {
-  // console.log("hello")
-  if (guess.value == isNaN(NaN)) {
+// print the messages in the card
+button.addEventListener("click", () => {
+  let number = parseInt(guess.value);
+  count ++;
+  console.log(count);
+  attempts(count);
+  if (count < 3 && isNaN(number)) {
+    alert("You need to put a number!");
     console.log("You need to put a number!");
-    // console.log("hello again")
-  } else if (guess.value < randomNumb) {
+  } else if (count <= 3 && number < randomNumb) {
     higher();
-  } else if (guess.value > randomNumb) {
+  } else if (count <= 3 && number > randomNumb) {
     lower();
-  }
-
-  else {
+  } else if (count <= 3 && number == randomNumb) {
+    youWon();
+  } else {
     console.log("hello again");
     youLost();
-}
+  };
 });
