c5fd5f7416050889f402e07db5d78484239125ac
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-10 20:22:07 +0000

    Created a card with react

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+  return(
+    <div className="card">
+      <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+      <h2 className="card-title">This is a card</h2>
+      <p className="card-text">Created with React</p>
+    </div>
+  );
+}
+
+export default Card
