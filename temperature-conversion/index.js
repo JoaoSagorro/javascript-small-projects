005215aba897657323fcac279132c7c4594c29f4
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-10 09:24:25 +0000

    added images and temperature logic

--- temperature-conversion/index.js
+++ temperature-conversion/index.js
@@ -1,5 +1,6 @@
 const temperature = document.querySelector("#temperature");
 // console.log(temperature);
+const tempDiv = document.querySelector("#temp-char");
 
 const submit = document.querySelector("#button");
 const celToFar = document.querySelector("#cel-far");
@@ -10,22 +11,27 @@ const celToKel = document.querySelector("#cel-kel");
 // console.log(celToKel.checked);
 const kelToCel = document.querySelector("#kel-cel");
 // console.log(kelToCel.checked);
+const result = document.getElementById("result");
 
 
 const celsiusToFahrenheit = (degree) => {
-  return degree * 1.8 + 32;
+  result.textContent = Math.floor(degree * 1.8 + 32) + "º F";
+  checkTemperature(degree);
 };
 
 const fahrenheitToCelsius = (degree) => {
-  return (degree - 32) * (5/9);
+  result.textContent = Math.floor((degree - 32) * (5/9)) + "º C";
+  checkTemperature(Math.floor((degree - 32) * (5/9)));
 };
 
 const celsiusToKelvin = (degree) => {
-  return degree + 273.15;
+  result.textContent = Math.floor(degree + 273.15) + " K";
+  checkTemperature(degree);
 };
 
 const kelvinToCelsius = (degree) => {
-  return degree - 273.15;
+  result.textContent = Math.floor(degree - 273.15) + "º C";
+  checkTemperature(Math.floor(degree - 273.15));
 };
 
 const convertTemperature = (input) => {
@@ -36,6 +42,26 @@ const convertTemperature = (input) => {
   else if (farToCel.checked) {
     fahrenheitToCelsius(input);
     console.log(fahrenheitToCelsius(input));
+  }
+  else if (celToKel.checked) {
+    celsiusToKelvin(input);
+    console.log(celsiusToKelvin(input));
+  }
+  else if (kelToCel.checked) {
+    kelvinToCelsius(input);
+    console.log(kelvinToCelsius(input));
+  };
+};
+
+const checkTemperature = (input) => {
+  if (input < 10) {
+    tempDiv.innerHTML = `<img src="Components/Images/cold.png" alt="cold" width="100px">`;
+  }
+  else if (input >= 10 && input < 25) {
+    tempDiv.innerHTML = `<img src="Components/Images/cool.png" alt="cold" width="100px"></img>`;
+  }
+  else {
+    tempDiv.innerHTML = `<img src="Components/Images/hot.png" alt="cold" width="100px">`
   }
 };
 
